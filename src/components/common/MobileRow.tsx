bed20dd194a575421be63584eba8a65644d31a78
Gobinda Das <gobinda168@gmail.com>
2021-02-02 13:04:48 +0530

    Update: minor ui changes

--- src/components/common/MobileRow.tsx
+++ src/components/common/MobileRow.tsx
@@ -1,12 +1,21 @@
 import styled from '@emotion/styled';
 
 const MobileRow = styled.div(
-  (props: { spacing?: string; justifyContent?: string; margin?: string }) => ({
+  (props: {
+    spacing?: string;
+    justifyContent?: string;
+    mJustifyContent?: string;
+    margin?: string;
+  }) => ({
     display: 'flex',
     justifyContent: props.justifyContent || 'flex-start',
     alignItems: 'center',
     div: {
       margin: props.spacing,
+    },
+    '@media(max-width:500px)': {
+      justifyContent: props.mJustifyContent,
+      div: { marginRight: '.4rem' },
     },
   })
 );
